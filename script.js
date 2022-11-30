c0f15ab90c4aa531fde47c39e2bd6cee805daaac
imhereforfuniguess <106017717+imhereforfuniguess@users.noreply.github.com>
2022-11-30 22:57:29 +0200

    Implement reset button, change grid margins to -1

--- script.js
+++ script.js
@@ -1,18 +1,55 @@
 
 const container = document.querySelector(".gridContainer");
+const resetter = document.querySelector ('button');
+let xAmount;
 
-
-
-for (let i = 0; i < 16; i++){
-    const height = document.createElement('div');
-    height.style.display = 'flex';
-    container.appendChild(height);
-    
-    for (let j = 0; j < 16; j++){
-        const coordinate = document.createElement('div');
-        coordinate.style.height =" 10px";
-        coordinate.style.width =" 10px";
-        height.appendChild(coordinate);
+resetter.addEventListener('click', function(e){
+    clearGrid();
+    do {
+        xAmount = prompt("Enter X, get X by X grid");
     }
+    while ((xAmount > 25))
+    makeGrid();
+});
 
+function clearGrid(){
+    console.log(xAmount);
+    for (let i = 0; i < xAmount; i++){
+        const height = document.querySelector(".height");
+        for (let j = 0; j < xAmount; j++){
+            const coordinate = document.querySelector('.coord');
+            height.removeChild(coordinate);
+        };
+        container.removeChild(height);
+    }
+    return;
 }
+
+
+function makeGrid(){
+    for (let i = 0; i < xAmount; i++){
+        const height = document.createElement('div');
+        height.setAttribute("class", "height");
+        height.style.display = 'flex';
+        container.appendChild(height);
+        
+        for (let j = 0; j < xAmount; j++){
+            const coordinate = document.createElement('div');
+
+            coordinate.setAttribute("class", "coord");
+            coordinate.style.height = `${500 / xAmount}px`;
+            coordinate.style.width = `${500 / xAmount}px`;
+
+            height.appendChild(coordinate);
+
+            coordinate.addEventListener('mouseover', () => {
+
+                coordinate.style.backgroundColor ="lightblue";
+
+            });
+
+        }
+    }
+}
+
+
